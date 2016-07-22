f58c61b8443b738ccdb9523fbbf5848090aa3c20
Mitch Allen <mitch.c.allen@gmail.com>
2016-07-22 10:49:33 -0700

    added heartbeat example

---
+++ examples/heartbeat/README.md
@@ -0,0 +1,25 @@
+examples / hearbeat
+===================
+
+Demo implementing a heartbeat service
+-------------------------------------------------
+
+### Install
+
+Because the __package.json__ file should already contain what you need, simply use this command to install everything:
+
+    $ npm install
+
+### Install and run the app
+
+From your projects root folder, execute the following at the command line:
+
+    $ node index.js
+
+### Test the app using curl commands
+
+TODO
+
+### Test the app in the browser
+
+TODO
---
+++ examples/heartbeat/index.js
@@ -0,0 +1,76 @@
+/**
+   Project: examples/heartbeat
+    Author: Mitch Allen
+
+   To test:
+
+   1. in a terminal window type:
+
+       node index.js
+
+   2. In a second terminal window type:
+
+       curl -i -X GET -H "Content-Type: application/json" http://localhost:8001/v1/heartbeat
+
+   3. Or in Chrome, browse to:
+
+       http://localhost:8001/v1/heartbeat
+*/
+
+/*jslint es6 */
+
+"use strict";
+
+// Define a Service object
+var service = {
+
+    // Get the name and version from package.json
+    name: require("./package").name,
+    version: require("./package").version,
+
+    // Turn on console message
+    verbose: true,
+
+    // Get API version from env or use default if null
+    // Used in URL, such as http://localhost:8001/{API-VERSION}/heartbeat
+    apiVersion: process.env.API_VERSION || '/v1',
+
+    // Get the port to listen on from env or use default if null
+    port: process.env.HEARTBEAT_PORT || 8001,
+
+    // microservice-core will pass an object containing an ExpressJS router.
+    // Use the router to define HTTP handlers
+    method: function (info) {
+
+        // Get the Express.JS router from the options
+        var router = info.router;
+
+        // Add an HTTP GET handler to the router
+        router.get('/heartbeat', function (req, res) {
+            // Specifiy a JSON formatted response
+            var data = {
+                type: "heartbeat",
+                status: "OK",
+                message: 'service is running',
+                timestamp: new Date(Date.now())
+            };
+            // Return the JSON response
+            res.json(data);
+        });
+
+        // Return the router (required).
+        return router;
+    }
+};
+
+// Create an options object
+var options = {
+    // Add the service object to the options object
+    service: service
+};
+
+// Pass the options to microservice-core
+module.exports = require('@mitchallen/microservice-core')(options);
+
+// microservice should not be listening on the port
+// Test with Chrome browser or curl command
